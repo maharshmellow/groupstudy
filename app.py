c71aa581e85d9cd3c004d8a0dd4f270eadd502e6
Maharsh Patel <patel@maharsh.net>
2017-08-22 17:00:35 -0600

    sync lag fix

--- app.py
+++ app.py
@@ -29,23 +29,34 @@ def index():
 @app.route('/<room>')
 def add_user(room):
     session["room_number"] = room
+    print(room)
     return render_template('index.html', async_mode=socketio.async_mode)
 
 
 @socketio.on('connect', namespace='/process')
 def connect():
     if not session["room_number"]:
-        # session["room_number"] =
         session['room_number'] = request.sid
+        emit('room_number_response', {"room_number":session["room_number"]})
     else:
-        # auto join the room
-        join({"room":session["room_number"]})
+        # auto join the room from the url
+        # leave_room(request.sid) # leave the room associtated with this user - joining group room instead
+        # emit('room_number_response', {"room_number":session["room_number"]})
+        # join({"room":session["room_number"]})
+        # get the time from the group
+        # emit('sync_time_request', room=session["room_number"])
+        # socketio.sleep(3)
 
-    emit('room_number_response', {"room_number":request.sid})
+        emit('response',{'data': "Someone joined room #"+session["room_number"]},room=session['room_number'])
+        socketio.sleep(1)
+        emit('sync_time_request', room=session["room_number"])
+        join_room(session["room_number"])
 
 
 @socketio.on('disconnect', namespace='/process')
 def disconnect():
+    leave_room(session["room_number"])
+    print(request.sid, "left room", session["room_number"])
     emit('response',
          {"data":"Someone Disconnected"},
          room=session["room_number"])
@@ -74,24 +85,24 @@ def broadcast_message(message):
     emit('response',{'data': message['data']}, broadcast=True)
 
 
-@socketio.on('join_event', namespace='/process')
-def join(message):
-    room = message["room"]
-    session["room_number"] = room
-    # send_room_message({"data":"new user requesting time", "room":message['room']})
-    emit('time_request', room=message["room"])
-    join_room(message['room'])
-    emit('response', {'data': 'In rooms: ' + ', '.join(rooms())})
-
-    send_room_message({"data":"Someone has joined the room", "room":message['room']})
-
-
-@socketio.on('room_event', namespace='/process')
-def send_room_message(message):
-    # session['receive_count'] = session.get('receive_count', 0)
-    emit('response',
-         {'data': message['data']},
-         room=message['room'])
+# # @socketio.on('join_event', namespace='/process')
+# def join(message):
+#     # room = message["room"]
+#     session["room_number"] = room
+#     # send_room_message({"data":"new user requesting time", "room":message['room']})
+#     emit('sync_time_request', room=message["room"])
+#     join_room(message['room'])
+#     emit('response', {'data': 'In rooms: ' + ', '.join(rooms())})
+#
+#     send_room_message({"data":"Someone has joined the room", "room":message['room']})
+#
+#
+# @socketio.on('room_event', namespace='/process')
+# def send_room_message(message):
+#     # session['receive_count'] = session.get('receive_count', 0)
+#     emit('response',
+#          {'data': message['data']},
+#          room=message['room'])
 
 
 @socketio.on('my_ping', namespace='/process')
